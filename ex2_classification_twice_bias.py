78bf48bbcccf52e13e8cdbbd63987f52665e9a53
Hans Andersen <hemae2468@gmail.com>
2021-02-26 06:14:19 +0500

    lesson 2.3 done (twice bias)

--- ex2_classification_twice_bias.py
+++ ex2_classification_twice_bias.py
@@ -2,14 +2,12 @@ import numpy as np
 
 
 def act(x):
-    return 0 if x < 0.5 else 1
+    return 0 if x < 0 else 1
 
-def go(vect):
+def go(vect, w1, w2, w11, c):
     x = np.array(vect)
-    w11 = [0.3, 0.3, 0]
-    w12 = [0.4, -0.5, 1]
-    weight1 = np.array([w11, w12]) # матрица 2х3
-    weight2 = np.array([-1, 1]) # вектор 1х2
+    weight1 = np.array([w1, w2]) # матрица 2х3
+    weight2 = np.array([w11[0], w11[1]]) # вектор 1х3
 
     sum_hidden = np.dot(weight1, x) # вычисляем сумму на нейронах срытого слоя
     print("Суммы на нейронах сткрытого слоя: " + str(sum_hidden))
@@ -17,16 +15,40 @@ def go(vect):
     out_hidden = np.array([act(x) for x in sum_hidden]) # считаем функцию активации на каждой сумме
     print("Значения функции активации на выходе нейронов скрытого слоя" + str(out_hidden))
 
-    sum_end = np.dot(weight2, out_hidden)
+    sum_end = np.dot(weight2, out_hidden) + w11[2] * c
     y = act(sum_end)
 
     if y == 0:
-        print("Облом")
+        print("C2")
     elif y == 1:
-        print("Фак е")
+        print("C1")
     else:
         print("Перепроверь прогу, чувак")
 
+b1 = 1.5
+b2 = 0.5
+xb = 1
+c = 1
+z = -0.5
 
-init_vect = [1, 1, 0]
-go(init_vect)
+w11 = 1
+w21 = w11
+w31 = -w21 * b1 / xb
+
+w12 = 1
+w22 = w12
+w32 = -w22 * b2 / xb
+
+weight11 = [w11, w21, w31]
+weight21 = [w12, w22, w32]
+
+wa = -1
+wb = 1
+wc = z / c
+
+weight12 = [wa, wb, wc]
+
+x = 0
+y = 0
+init_vect = [0, 1, xb]
+go(init_vect, weight11, weight21, weight12, c)
