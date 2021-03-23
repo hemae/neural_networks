1efb2743e6bf477e9a0ab70b4767f04b9759ff6d
Hans Andersen <hemae2468@gmail.com>
2021-03-24 04:21:05 +0500

    my neural network

--- my_neural_network.py
+++ my_neural_network.py
@@ -1,17 +1,57 @@
 import numpy as np
 
 
+def linear(x, a=1):
+    return a * x
+
+def der_linear(x, a=1):
+    return a
+
+def sigmoida(x):
+    return 1 / (1 + np.exp(-x))
+
+def der_sigmoida(x):
+    return sigmoida(x) * (1 - sigmoida(x))
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+def der_tanh(x):
+    return 1 - (tanh(x)) * (tanh(x))
+
 def relu(x, a=1):
-    if x <= a:
+    if x < a:
         return 0
     else:
         return x
+
+def der_relu(x, a=1):
+    if x < a:
+        return 0
+    else:
+        return 1
+
+funcs = {
+    'one': {
+        'linear': linear,
+        'sigmoida': sigmoida,
+        'tanh': tanh,
+        'relu': relu
+    },
+    'der': {
+        'linear': der_linear,
+        'sigmoida': der_sigmoida,
+        'tanh': der_tanh,
+        'relu': der_relu
+    }
+}
+
 
 def neuron(input_signal, activation_func=None):
     if activation_func == None:
         return input_signal
     else:
-        return activation_func(input_signal)
+        return funcs['one'][activation_func]
 
 def layer(neurons_number, input_vector, weight_vector, activation_func=None):
     output_vector = []
@@ -41,12 +81,21 @@ def initialize_total_weight_vector(network_scheme):
 
     return total_weight_vector
 
+def back_propagation_iteration(network_scheme, total_weight_vector, error, conv_step, activation_func, activation_func_output):
+    new_total_weight_vector = []
+    reverse_network_scheme = network_scheme[::-1]
+    input_vector = error * funcs['der'][activation_func_output]
+
+
+    return new_total_weight_vector
+
 
 network_scheme = [3, 1]
 input_vector = [-1, 1, -1]
 total_weight_vector = initialize_total_weight_vector(network_scheme)
 # print(total_weight_vector)
 
-forward_propagation = network(network_scheme, input_vector, total_weight_vector)
+forward_propagation = network(network_scheme, input_vector, total_weight_vector, activation_func='linear')
 print(forward_propagation)
+
 
